b30ff3eca75e2af919c8cc6043846f768242eb01
Senior Python Developer (SEPYDEV) <freelancer.programmer.2020@gmail.com>
2022-11-12 19:36:21 +0330

    Convert chr to ASCII-Unicode and vice versa

---
+++ text/practice2.py
@@ -0,0 +1,10 @@
+# Convert char to ASCII - Unicode
+print(ord('a'))
+print(ord('ع'))
+
+# Convert ASCII - Unicode to char
+print(chr(97))
+print(chr(1593))
+
+# unichar does not exist in python 3
+# chr method convert ASCII and Unicode to character
