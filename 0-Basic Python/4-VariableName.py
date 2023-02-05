b30669addad9be1e90ae86eb5ea24389c4f263ab
Back-end programmer <freelancer.programmer.2020@gmail.com>
2023-02-05 21:28:40 +0200

    variable and assignment
    use values and assignment value to variable and you learn how to use packed and unpacked value
    how to use Name of variables

---
+++ 0-Basic Python/4-VariableName.py
@@ -0,0 +1,27 @@
+"""
+How to set variable names?
+
+1- A variable name must start with a letter or the underscore character 
+2- A variable name cannot start with a number
+3- A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+4- Variable names are case-sensitive (age, Age and AGE are three different variables)
+
+"""
+mytestvar = "THIS IS A TEST"
+my_test_var = "THIS IS A TEST"
+_my_test_var = "THIS IS A TEST"
+myTestVar = "THIS IS A TEST"
+MYTESTVAR = "THIS IS A TEST"
+mytestvar2 = "THIS IS A TEST"
+
+# Error
+# this code has  Error why ?
+"""
+2myvar = "John"
+my-var = "John"
+my var = "John"
+"""
+
+myVariableName = "THIS IS FOR TEST"  # Camel Case
+MyVariableName = "THIS IS FOR TEST"  # Pascal Case
+my_variable_name = "THIS IS FOR TEST"  # Snake Case
