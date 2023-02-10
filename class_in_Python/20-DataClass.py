d19f1d7c63ca5675412588afde09524e20bb57d6
Back-end programmer <freelancer.programmer.2020@gmail.com>
2023-02-10 14:56:34 +0200

    Update 20-DataClass.py
    If you have a class that stores data and want to easily create
    The best way is to use the data class

--- class_in_Python/20-DataClass.py
+++ class_in_Python/20-DataClass.py
@@ -1,17 +1,25 @@
+# In Python, a data class is a class designed to store data. It is typically used to store data structures t
+# hat can be represented as a collection of attributes. A data class is typically simpler
+#  and more lightweight compared to a regular class, as it does not include any logic or methods
+#  beyond those necessary to store and manage its data.
+
+
 from dataclasses import dataclass
-#this class only save data and dosent have methods
+# this class only save data and dosent have methods
+
+
 @dataclass
 class Employee:
     name: str
     family: str
     type: str
     salary: float
-    age  :int
+    age: int
 
 
-employee = Employee("soheila","nazeri",salary= 50000,age=37,type=1)
-print(employee.name)
-print(employee.family)
-print(employee.salary)
-print(employee.age)
-print(employee.type )
+employee = Employee("soheila", "nazeri", salary=50000, age=37, type=1)
+print("name is :", employee.name)
+print("family is :", employee.family)
+print("salary is :", employee.salary)
+print("age is :", employee.age)
+print("type is :", employee.type)
